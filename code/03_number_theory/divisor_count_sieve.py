32d35141c80219fb36dc3d128c8f9d145dad519c
madnan63 <shaadnan1400@gmail.com>
2026-09-12 13:38:45 +0600

    div count sieve update

--- code/03_number_theory/divisor_count_sieve.py
+++ code/03_number_theory/divisor_count_sieve.py
@@ -7,7 +7,7 @@ for i in range(1, N + 1):
         divisor_count[j] += 1
 
 # For sum of divisors instead:
-# divisor_sum = [0] * (N + 1)
-# for i in range(1, N + 1):
-#     for j in range(i, N + 1, i):
-#         divisor_sum[j] += i
+divisor_sum = [0] * (N + 1)
+for i in range(1, N + 1):
+    for j in range(i, N + 1, i):
+        divisor_sum[j] += i
